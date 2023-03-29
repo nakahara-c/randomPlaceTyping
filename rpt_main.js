50c3c1d1ae90757b8a2f499d0064d719ead0d05c
nakahara-c <ttcocoan@gmail.com>
2023-03-30 00:42:40 +0900

    キー入力受けるいべりす

--- rpt_main.js
+++ rpt_main.js
@@ -1,1 +1,26 @@
-console.log(1);
+import { wordList } from "./wordList";
+
+let time = 0;
+
+window.addEventListener('keydown', startType);
+function startType(e) {
+    if (e.key === 'Enter' || e.key === ' ' || e.key === 'Escape') {
+
+        /*
+        timer.textContent = "0.0";
+        countChars.textContent = "0";
+        countTypes.textContent = "0";
+        kpm.textContent = "0";
+        cpm.textContent = "0";
+        char.textContent = "";
+        num.textContent = "";
+        progress.value = 0;
+        */
+        time = 0;
+
+        makeWord();
+        stopInterval();
+        timerArray.push(setInterval(startTimer, 100));
+        window.addEventListener('keydown', judgeKeys);
+    }
+}
